a94d21db6087f13f0f8d39243a22615f4d1047cf
Mitchel Kelonye <kelonyemitchel@gmail.com>
2013-08-23 20:43:57 +0300

    refactored setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='kelonyemitchel@gmail.com',
     url='https://github.com/kelonye/django-hash-password',
     requires=['Django (>=1.3.0)'],
-    packages=find_packages(),
+    packages=['django_hash_password',],
+    package_dir = {'django_hash_password': 'lib'},
     license='MIT License',
     zip_safe=True)
